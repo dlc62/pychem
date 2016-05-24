73adc156ceac52b538c17870e2acebd8613d171a
Chris Stinson <chris.stinson202@gmail.com>
2016-05-24 12:16:08 +1200

    Example input file

--- example_input.py
+++ example_input.py
@@ -2,6 +2,9 @@
 BasisSets = ['STO-3G']
 Multiplicity = 2
 Charge = 0
+Coords = [['H',1.0,  0.  ,  0.,   0.],
+          ['H',1.0,  1.0 ,  0.,   0.],
+          ['H',1.0,  0.4,  1.0,   0.]]
 Excitations = [[-1,1]]
 direct = False
 reference = "cuhf"
@@ -14,7 +17,9 @@ MOFileWrite = "MOs.out"
 # A second calculation
 [calc2]
 BasisSets = ['STO-3G', '321G']
+Coords = [['Li',3,0,0,0],['H',1,0,0,1.599427]]
 Multiplicity = 1
 Charge = 0
 Excitations = [[-1,1]]
 UseDISS = True
+SCFGuess = "core"
